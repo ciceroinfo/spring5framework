22f41505ed9ccd41d5a377f2f3c751989ddc1096
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-07-20 22:06:36 -0300

    Chuck Norris Jokes initial

---
+++ chuck-norris-joke/src/main/java/com/cicero/springframework/ChuckNorrisJokeApplication.java
@@ -0,0 +1,12 @@
+package com.cicero.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisJokeApplication.class, args);
+	}
+}
