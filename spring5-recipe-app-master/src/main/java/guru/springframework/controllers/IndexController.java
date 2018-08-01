31d326798e1c5ecfbb184636b8436e8f3ffd1887
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-07-31 21:59:27 -0300

    recipe init

---
+++ spring5-recipe-app-master/src/main/java/guru/springframework/controllers/IndexController.java
@@ -0,0 +1,18 @@
+package guru.springframework.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 
+ * @author cicero
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping({"", "/", "/index"})
+	public String getIndexPage() {
+		return "index";
+	}
+}
