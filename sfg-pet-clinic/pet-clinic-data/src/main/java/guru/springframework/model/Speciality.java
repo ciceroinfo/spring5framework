7762c34f43d27512a65a31dab0157f60624ecc59
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-08-15 08:01:56 -0300

    add entity jpa on speciality and vet

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/model/Speciality.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/model/Speciality.java
@@ -1,9 +1,16 @@
 package guru.springframework.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Column(name = "description")
 	private String description;
 
 	public String getDescription() {
