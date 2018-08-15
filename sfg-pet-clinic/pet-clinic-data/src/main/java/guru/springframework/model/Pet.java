297edcbbe3fb3fd94d62ab558643a3d1e4c571c4
LAPTOP-CICERO\cicero <cicero.info@gmail.com>
2018-08-15 08:09:27 -0300

    add entity on pet and visit

--- sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/model/Pet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/guru/springframework/model/Pet.java
@@ -1,11 +1,15 @@
 package guru.springframework.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pet extends BaseEntity {
@@ -25,6 +29,9 @@ public class Pet extends BaseEntity {
 	
 	@Column(name = "birth_date")
 	private LocalDate birthdate;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+	private Set<Visit> visits = new HashSet<>();
 
 	public String getName() {
 		return name;
@@ -56,6 +63,14 @@ public class Pet extends BaseEntity {
 
 	public void setBirthdate(LocalDate birthdate) {
 		this.birthdate = birthdate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 
 }
